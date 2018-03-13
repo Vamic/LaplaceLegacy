f02ce1cf25532bf64f5291e61152a9037b0dd9ba
Carl König <made.swede@live.com>
2018-03-13 08:45:39 +0100

    !define

---
+++ plugins/urbandictionary.js
@@ -0,0 +1,29 @@
+﻿var bot = module.parent.exports;
+
+exports.commands = {
+    define: {
+        commands: ["!define", "!what's", "!whats"],
+        exec: function (command, message) {
+            if (!command.arguments.length) {
+                message.channel.send(info.channel, "Huh. Give me something to define.");
+                return;
+            }
+            bot.util.httpGetJson("http://api.urbandictionary.com/v0/define?term=" +
+                encodeURIComponent(command.arguments.join(" ")), function (err, data) {
+                    if (err) {
+                        message.channel.send("Something went wrong.");
+                        return;
+                    }
+                    if (data.list.length < 1) {
+                        message.channel.send("Not even I know what that is! Are you sure it exists? :0");
+                        return;
+                    }
+
+                    var text = "**[" + data.list[0].word + "]**\n\n" + data.list[0].definition;
+
+                    message.channel.send(text);
+                }
+            );
+        }
+    }
+};
